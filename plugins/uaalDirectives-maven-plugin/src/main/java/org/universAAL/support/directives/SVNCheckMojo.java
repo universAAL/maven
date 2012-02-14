1a8372a2261211f30bb1543cae147458a6ec3f6e
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2012-02-14 23:45:19 +0000

    working svn-check

--- plugins/uaalDirectives-maven-plugin/src/main/java/org/universAAL/support/directives/SVNCheckMojo.java
+++ plugins/uaalDirectives-maven-plugin/src/main/java/org/universAAL/support/directives/SVNCheckMojo.java
@@ -18,7 +18,9 @@ package org.universAAL.support.directives;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
 import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNURL;
 import org.tmatesoft.svn.core.wc.SVNClientManager;
 import org.tmatesoft.svn.core.wc.SVNStatus;
 
@@ -30,20 +32,53 @@ import org.tmatesoft.svn.core.wc.SVNStatus;
  * @phase process-sources
  */
 public class SVNCheckMojo
-    extends AbstractMojo
+extends AbstractMojo
 {
-	
-	/** @parameter default-value="${project}" */
-	private org.apache.maven.project.MavenProject mavenProject;
+
+    /**
+     * Message content when check fails
+     */
+    private static final String SCM_NOT_CONFIGURED = "It seems the POM does not contain a SCM tag, or the SCM does not match the actual SVN repository URL.";
+    
+    /** @parameter default-value="${project}" */
+    private org.apache.maven.project.MavenProject mavenProject;
 
 
-    public void execute() throws MojoExecutionException {
-    	try {
-			SVNStatus status = SVNClientManager.newInstance().getStatusClient().doStatus(mavenProject.getBasedir(), false);
-			System.out.println(status.getRemoteURL().toDecodedString());
-		} catch (SVNException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+    public void execute() throws MojoFailureException {
+
+	SVNClientManager cli = SVNClientManager.newInstance();
+	SVNStatus status;
+	this.getLog().debug("checking svn for " + mavenProject.getBasedir().getPath());
+	try {
+	    status = cli.getStatusClient().doStatus(mavenProject.getBasedir(), false);
+	    if (status != null) {
+		getLog().debug("not null Status");
+		SVNURL url = status.getURL();
+		if (url != null){
+		    String surl = url.toDecodedString();
+		    getLog().debug("found URL	: " + surl);
+		    getLog().debug("comparing with	: " + mavenProject.getScm().getConnection());
+		    getLog().debug("comparing with	: " + mavenProject.getScm().getDeveloperConnection());
+		    if (!mavenProject.getScm().getConnection().endsWith(surl)
+			    && !mavenProject.getScm().getDeveloperConnection().endsWith(surl)){
+			throw new MojoFailureException(SCM_NOT_CONFIGURED);
+		    }
+		    else{
+			getLog().info("SCM and SVN info are in sync.");
+		    }
 		}
+		else{
+		    getLog().error("unable to find URL from svn info.");
+		}
+	    }
+	    else{
+		getLog().warn("directory seems not to be a local SVN working copy.");
+	    }
+	} catch (SVNException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	    getLog().warn("SVN Error.");
+	}
+
     }
 }
