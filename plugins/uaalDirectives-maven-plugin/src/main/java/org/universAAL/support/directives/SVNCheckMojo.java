969f1b24c3a49cecaa900bedaf6c5a04130460da
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2012-02-18 15:51:56 +0000

    reorganization

--- plugins/uaalDirectives-maven-plugin/src/main/java/org/universAAL/support/directives/SVNCheckMojo.java
+++ plugins/uaalDirectives-maven-plugin/src/main/java/org/universAAL/support/directives/SVNCheckMojo.java
@@ -27,6 +27,7 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoFailureException;
 import org.tmatesoft.svn.core.SVNException;
 import org.tmatesoft.svn.core.SVNURL;
+import org.tmatesoft.svn.core.internal.wc.SVNErrorManager;
 import org.tmatesoft.svn.core.wc.SVNClientManager;
 import org.tmatesoft.svn.core.wc.SVNStatus;
 
@@ -39,115 +40,107 @@ import org.tmatesoft.svn.core.wc.SVNStatus;
  */
 public class SVNCheckMojo extends AbstractMojo {
 
-	private static final String NEW_URL = "forge.universAAL.org";
+    private static final String NEW_URL = "forge.universAAL.org";
 
-	private static final String OLD_URL = "a1gforge.igd.fraunhofer.de";
+    private static final String OLD_URL = "a1gforge.igd.fraunhofer.de";
 
-	/**
-	 * Message content when check fails
-	 */
-	private static final String SCM_NOT_CONFIGURED = System
-			.getProperty("line.separator")
-			+ "\n"
-			+ "SCM Conformance Directive Fail :\n"
-			+ "It seems the POM does not contain a SCM tag, "
-			+ "or the SCM does not match the actual SVN repository URL."
-			+ System.getProperty("line.separator") + "\n";
+    /**
+     * Message content when check fails
+     */
+    private static final String SCM_NOT_CONFIGURED = System
+	    .getProperty("line.separator")
+	    + "\n"
+	    + "SCM Conformance Directive Fail :\n"
+	    + "It seems the POM does not contain a SCM tag, "
+	    + "or the SCM does not match the actual SVN repository URL."
+	    + System.getProperty("line.separator") + "\n";
 
-	/**
-	 * @parameter expression="${failOnMissMatch}" default-value="false"
-	 */
-	private boolean failOnMissMatch;
+    /**
+     * @parameter expression="${failOnMissMatch}" default-value="false"
+     */
+    private boolean failOnMissMatch;
 
-	/**
-	 * @parameter expression="${directive.fix}" default-value="false"
-	 */
-	private boolean fixSCM;
+    /**
+     * @parameter expression="${directive.fix}" default-value="false"
+     */
+    private boolean fixSCM;
 
-	/** @parameter default-value="${project}" */
-	private org.apache.maven.project.MavenProject mavenProject;
+    /** @parameter default-value="${project}" */
+    private org.apache.maven.project.MavenProject mavenProject;
 
-	public void execute() throws MojoFailureException {
+    public void execute() throws MojoFailureException {
+	this.getLog().debug(
+		"checking svn for " + mavenProject.getBasedir().getPath());
+	try {
+	    String surl = getSVNURL(mavenProject.getBasedir());
 
-		SVNClientManager cli = SVNClientManager.newInstance();
-		SVNStatus status;
-		this.getLog().debug(
-				"checking svn for " + mavenProject.getBasedir().getPath());
-		try {
-			status = cli.getStatusClient().doStatus(mavenProject.getBasedir(),
-					false);
-			if (status != null) {
-				getLog().debug("not null Status");
-				SVNURL url = status.getURL();
-				if (url != null) {
-					String surl = url.toDecodedString();
-					if (!fixSCM) {
-						getLog().debug("found URL	: " + surl);
-						getLog().debug(
-								"comparing with	: "
-										+ mavenProject.getScm().getConnection());
-						getLog().debug(
-								"comparing with	: "
-										+ mavenProject.getScm()
-												.getDeveloperConnection());
-						if (missMatchURLs(surl)
-								|| missMatchURLs(surl.replace(OLD_URL, NEW_URL))) {
-							if (failOnMissMatch) {
-								throw new MojoFailureException(
-										SCM_NOT_CONFIGURED);
-							} else {
-								getLog().warn(SCM_NOT_CONFIGURED);
-							}
-						} else {
-							getLog().info("SCM and SVN info are in sync.");
-						}
-					}
-					else {
-						getLog().debug("Fixing SCM with URL: "+ surl);
-						fixSCMWith(surl);
-					}
-				} else {
-					getLog().error("unable to find URL from svn info.");
-				}
-			} else {
-				getLog().warn(
-						"directory seems not to be a local SVN working copy.");
-			}
-		} catch (SVNException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			getLog().warn("SVN Error.");
+	    getLog().debug("found URL	: " + surl);
+	    getLog().debug(
+		    "comparing with	: " + mavenProject.getScm().getConnection());
+	    getLog().debug(
+		    "comparing with	: "
+			    + mavenProject.getScm().getDeveloperConnection());
+	    if (missMatchURLs(surl)
+		    || missMatchURLs(surl.replace(OLD_URL, NEW_URL))) {
+		if (failOnMissMatch) {
+		    throw new MojoFailureException(SCM_NOT_CONFIGURED);
+		} else {
+		    getLog().warn(SCM_NOT_CONFIGURED);
 		}
-
-	}
-
-	private void fixSCMWith(String surl) {
-		mavenProject.getScm().setConnection(surl);
-		mavenProject.getScm().setDeveloperConnection(surl);
-		//TODO: rewrite POM with correct SCM.
-		try {
-		//Reading
-		MavenXpp3Reader reader = new MavenXpp3Reader();
-		Model model = reader.read(new FileInputStream(new File(mavenProject.getFile().getAbsolutePath())));
-
-		//Editing
-		model.setScm(mavenProject.getScm());
-
-		//Writing
-		MavenXpp3Writer writer = new MavenXpp3Writer();
-		
-			writer.write(new OutputStreamWriter( 
-					new FileOutputStream(
-							new File(mavenProject.getFile().getAbsolutePath()))), model);
-		} catch (Exception e) {
-			getLog().error("Unable to write POM");
+		if (fixSCM) {
+		    getLog().debug("Fixing SCM with URL: " + surl);
+		    fixSCMWith(surl);
 		}
+	    } else {
+		getLog().info("SCM and SVN info are in sync.");
+	    }
+	} catch (SVNException e) {
+	    getLog().warn("SVN Error.", e);
+	    getLog().warn("directory seems not to be a local SVN working copy.");
+	}
+	catch (Exception e1){
+	    getLog().error(e1);
 	}
 
-	
-	private boolean missMatchURLs(String url) {
-		return !mavenProject.getScm().getConnection().endsWith(url)
-				&& !mavenProject.getScm().getDeveloperConnection()
-						.endsWith(url);
+    }
+
+    private void fixSCMWith(String surl) {
+	mavenProject.getScm().setConnection(surl);
+	mavenProject.getScm().setDeveloperConnection(surl);
+	// rewrite POM with correct SCM.
+	try {
+	    // Reading
+	    MavenXpp3Reader reader = new MavenXpp3Reader();
+	    Model model = reader.read(new FileInputStream(new File(mavenProject
+		    .getFile().getAbsolutePath())));
+
+	    // Editing
+	    model.setScm(mavenProject.getScm());
+
+	    // Writing
+	    MavenXpp3Writer writer = new MavenXpp3Writer();
+
+	    writer.write(new OutputStreamWriter(new FileOutputStream(new File(
+		    mavenProject.getFile().getAbsolutePath()))), model);
+	} catch (Exception e) {
+	    getLog().error("Unable to write POM");
 	}
+    }
+
+    private boolean missMatchURLs(String url) {
+	return !mavenProject.getScm().getConnection().endsWith(url)
+		&& !mavenProject.getScm().getDeveloperConnection()
+			.endsWith(url);
+    }
+
+    public static String getSVNURL(File dir) throws Exception, SVNException {
+	SVNClientManager cli = SVNClientManager.newInstance();
+	SVNStatus status;
+	status = cli.getStatusClient().doStatus(dir, false);
+	if (status != null) {
+	    SVNURL url = status.getURL();
+	    return url.toDecodedString();
+	}
+	throw new Exception("unable to find URL from svn info.");
+    }
 }
