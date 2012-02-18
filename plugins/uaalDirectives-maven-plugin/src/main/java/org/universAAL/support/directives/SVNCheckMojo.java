159809dbb9f57017ade622f8ffb93dd266d72354
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2012-02-18 16:44:38 +0000

    added variable check Directive on root POMs

--- plugins/uaalDirectives-maven-plugin/src/main/java/org/universAAL/support/directives/SVNCheckMojo.java
+++ plugins/uaalDirectives-maven-plugin/src/main/java/org/universAAL/support/directives/SVNCheckMojo.java
@@ -27,7 +27,6 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoFailureException;
 import org.tmatesoft.svn.core.SVNException;
 import org.tmatesoft.svn.core.SVNURL;
-import org.tmatesoft.svn.core.internal.wc.SVNErrorManager;
 import org.tmatesoft.svn.core.wc.SVNClientManager;
 import org.tmatesoft.svn.core.wc.SVNStatus;
 
