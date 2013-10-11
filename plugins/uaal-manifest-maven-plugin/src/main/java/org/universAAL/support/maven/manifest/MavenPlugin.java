e100f9675db3bbcc22b976e1240d9f224088efbc
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-10-11 00:25:21 +0000

    manifest maven plugin

--- plugins/uaal-manifest-maven-plugin/src/main/java/org/universAAL/support/maven/manifest/MavenPlugin.java
+++ plugins/uaal-manifest-maven-plugin/src/main/java/org/universAAL/support/maven/manifest/MavenPlugin.java
@@ -1,28 +1,25 @@
+/*	
+	Copyright 2007-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer-Gesellschaft - Institute for Computer Graphics Research
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
 package org.universAAL.support.maven.manifest;
 
-/*
- * Copyright 2001-2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-import java.util.LinkedList;
-import java.util.List;
-import java.util.jar.Manifest;
-
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.w3c.dom.Document;
 
 /**
  * @goal uaalManifest
@@ -40,8 +37,7 @@ public class MavenPlugin extends AbstractMojo {
      */
     private String output;
 
-    @Override
-    public void execute() throws MojoExecutionException {
+    public void execute() {
 	ManifestReader reader = new ManifestReader(input);
 	getLog().debug("Reading File '" + input + "'");
 	reader.read();
@@ -59,17 +55,10 @@ public class MavenPlugin extends AbstractMojo {
 	input = uaalPath;
     }
 
-    
-    
-    public static void main(String args[]) {
-	//Manifest m = new Manifest();
-	MavenPlugin p = new MavenPlugin();
-	p.setUaalManifestPath("uaal-manifest.xml");
-	p.setManifestPath("uaal-manifest.mf");
-	try {
-	    p.execute();
-	} catch (MojoExecutionException e) {
-	    e.printStackTrace();
-	}
-    }
+    // public static void main(String args[]) {
+    // MavenPlugin p = new MavenPlugin();
+    // p.setUaalManifestPath("uaal-manifest.xml");
+    // p.setManifestPath("uaal-manifest.mf");
+    // p.execute();
+    // }
 }
