4a2237d5e14ad7dcf55d13989e64afc42892b205
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2013-11-12 21:10:59 +0000

    made the module detection more smart and flexible.

--- plugins/uaalDirectives-maven-plugin/src/main/java/org/universAAL/support/directives/checks/ModulesCheckFix.java
+++ plugins/uaalDirectives-maven-plugin/src/main/java/org/universAAL/support/directives/checks/ModulesCheckFix.java
@@ -17,6 +17,8 @@ package org.universAAL.support.directives.checks;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.io.IOException;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -91,16 +93,105 @@ public class ModulesCheckFix implements APIFixableCheck, PomFixer {
 	 * @return
 	 */
 	private boolean passCheck(MavenProject mavenProject2, Log log) {
-		if (mavenProject2.getPackaging().equals("pom")) {
-			return passRootCheck(mavenProject2, log);
+		if (isAparentPOM(mavenProject2, log)) {
+		    if (isOfCommonType(mavenProject2, log)){
+			return passRootCheckCommonType(mavenProject2, log);
+		    }
+		    else {
+			return passRootCheckSiblingType(mavenProject2, log);
+		    }
 		}
 		else {
 			return true;
 		}
 	}
+	
+	/**
+	 * Checks the project is a POM and a parent POM (ie it has no source).
+	 * @param mavenProject2
+	 * @param log TODO
+	 * @return
+	 */
+	private boolean isAparentPOM(MavenProject mavenProject2, Log log) {
+	    boolean isAPOM = mavenProject2.getPackaging().equals("pom");
+	    for (File f : mavenProject2.getBasedir().listFiles()) {
+		String rel = f.getName();
+		if (f.isDirectory()
+				&& !rel.contains(SVN_FOLDER)
+				// it is not the svn folder
+				) {
+			isAPOM = !f.getName().contains("src");
+			log.debug(f.getName());
+		}
+	}
+	    return isAPOM;
+	}
+
+	/**
+	 * Check whether the pom modules are in the following disposition:
+	 * <br>
+	 * <pre>
+	 * 	parentFolder
+	 * 	L pom.xml
+	 * 	L Module1
+	 * 	  L pom.xml
+	 * 	:
+	 * 	L ModuleN
+	 * 	  L pom.xml
+	 * </pre>
+	 * ie: all of the modules of the pom are children.
+	 * @param mavenProject2
+	 * @param log
+	 * @return
+	 */
+	private boolean isOfCommonType(MavenProject mp, Log log) {
+	    List<String> listed = (List<String>) mp.getModules();
+	    boolean common = true;
+	    for (String m : listed) {
+		File mDir = new File(mp.getBasedir(),m);
+		log.debug("checking \n -" + mp.getBasedir().getAbsolutePath() + "\n -"+ mDir.getAbsolutePath());
+		try {
+		    common &= mp.getBasedir().equals(mDir.getCanonicalFile().getParentFile());
+		} catch (IOException e) {
+		    log.error(e);
+		}
+		log.debug(Boolean.toString(common));
+
+	    }
+	    return listed.size() > 0 && common;
+	}
+	
 
 
-	private boolean passRootCheck(MavenProject mavenProject2, Log log) {
+	private boolean passRootCheckCommonType(MavenProject mavenProject2, Log log) {
+		toBeFixed = new ArrayList<String>();
+		List<String> mods = (List<String>) mavenProject2.getModules();
+		List<File> listed = new ArrayList<File>();
+		for (String m : mods) {
+		    try {
+			listed.add(new File(mavenProject2.getBasedir(),m).getCanonicalFile());
+		    } catch (IOException e) {
+			log.error(e);
+		    }
+		}
+		//gather the existent modules
+		File dir = mavenProject2.getBasedir();
+		for (File f : dir.listFiles()) {
+			String rel = f.getName();
+			if (f.isDirectory()
+					&& directoryContains(f, "pom.xml")
+					&& !listed.contains(f)	// it is not already listed
+					&& !rel.contains(SVN_FOLDER)
+					// it is not the svn folder
+					) {
+				toBeFixed.add(rel);
+				log.debug("Found not listed module : " + rel);
+			}
+		}
+		return toBeFixed.isEmpty();
+	}
+
+	private boolean passRootCheckSiblingType(MavenProject mavenProject2, Log log) {
 		toBeFixed = new ArrayList<String>();
 		List<String> listed = (List<String>) mavenProject2.getModules();
 		
